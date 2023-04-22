2bc93d0ca419d9a87a67b1ac83902b1c9896a761
Igor <egor@sgf.lt>
2023-04-22 15:19:10 +0300

    Add bar plots

--- main.R
+++ main.R
@@ -1,4 +1,52 @@
-install.packages("readxl")
-library(readxl)
+install.packages(c("readxl", "ggplot2"))
 
-bank_df <- read_xlsx(path = "data.xlsx", sheet = "2018")
+library(readxl)
+library(ggplot2)
+
+data <- read_xlsx(path = "data.xlsx", sheet = "2018")
+
+make_barplot <- function(data, names, title) {
+    table <- data.frame(Data = data, Countries = names)
+    sorted_data <- table[order(table$Data), ]
+    barplot(
+        height = sorted_data$Data,
+        names = sorted_data$Countries,
+        main = title,
+        xlab = "Percentage",
+        col = "#69b3a2",
+        cex.names = 0.6,
+        horiz = TRUE,
+        las = 2
+    )
+}
+
+make_barplot(
+    data = data$Y1,
+    names = data$Country,
+    title = "Early leavers from education and training by country"
+)
+make_barplot(
+    data = data$Y2,
+    names = data$Country,
+    title = "Tertiary education attainment by country"
+)
+make_barplot(
+    data = data$Y3,
+    names = data$Country,
+    title = "Early childhood education and care by country"
+)
+make_barplot(
+    data = data$Y4,
+    names = data$Country,
+    title = "Employment rates of recent graduates by country"
+)
+make_barplot(
+    data = data$Y5,
+    names = data$Country,
+    title = "Adult participation in learning by country"
+)
+make_barplot(
+    data = data$Y6,
+    names = data$Country,
+    title = "Underachievement in mathematics, reading and science by country"
+)
